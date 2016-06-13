74f94d129b81c7c48c0c09a664b5eceb95b18a77
Mate Juhasz <mate.juhasz@ge.com>
2016-06-13 11:10:14 +0200

    UserRepository interface and User entity added. Please enter the commit message for your changes. Lines starting

---
+++ src/main/java/entity/User.java
@@ -0,0 +1,29 @@
+package entity;
+
+/**
+ * Created by 212393105 on 2016.06.13..
+ */
+public class User {
+    private String userName;
+    private String password;
+
+    public User() {
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+}

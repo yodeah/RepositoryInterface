7a5b3845178263ddfa66b6e2ac8db3e979916c96
Aron Horvath <aron.horvath@ge.com>
2016-06-13 11:33:39 +0200

    ContactGroupRepository and entity

---
+++ src/main/java/entity/ContactGroup.java
@@ -0,0 +1,41 @@
+package entity;
+
+/**
+ * Created by 212564432 on 6/13/2016.
+ */
+public class ContactGroup {
+    private String name;
+    private String displayName;
+
+    public ContactGroup() {
+    }
+
+    public ContactGroup(String displayName, String name) {
+        this.displayName = displayName;
+        this.name = name;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    public void setDisplayName(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "ContactGroup{" +
+                "name= " + name +
+                ", displayName= " + displayName +
+                '}';
+    }
+}
---
+++ src/main/java/repository/ContactGroupRepository.java
@@ -0,0 +1,19 @@
+package repository;
+
+import entity.ContactGroup;
+
+import java.util.List;
+
+/**
+ * Created by 212564432 on 6/13/2016.
+ */
+public interface ContactGroupRepository {
+
+    ContactGroup save(ContactGroup contactGroup);
+
+    void delete(String contactId);
+
+    ContactGroup findOne (String contactId);
+
+    List<ContactGroup> findAll();
+}

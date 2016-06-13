fc3e8b3c8300f24c2843925c30b6ed24b78eb667
Mate Juhasz <mate.juhasz@ge.com>
2016-06-13 11:30:39 +0200

    Role attribute added to User entity

--- src/main/java/entity/User.java
+++ src/main/java/entity/User.java
@@ -6,8 +6,23 @@ package entity;
 public class User {
     private String userName;
     private String password;
+    private UserRole role;
 
     public User() {
+    }
+
+    public User(String userName, String password, UserRole role) {
+        this.userName = userName;
+        this.password = password;
+        this.role = role;
+    }
+
+    public UserRole getRole() {
+        return role;
+    }
+
+    public void setRole(UserRole role) {
+        this.role = role;
     }
 
     public String getUserName() {

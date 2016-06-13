d3c9c23a4dc02ccba9d28f2c22e02de01bc11458
Andras Nemeth <andrasjanos.nemeth@ge.com>
2016-06-13 13:17:24 +0200

    token impl

--- src/main/java/com/ge/academy/contact_list/entity/Token.java
+++ src/main/java/com/ge/academy/contact_list/entity/Token.java
@@ -1,7 +1,37 @@
 package com.ge.academy.contact_list.entity;
 
+import java.time.LocalDateTime;
+
 /**
  * Created by 212566307 on 6/13/2016.
  */
 public class Token {
+
+    private String tokenId;
+    private User user;
+    private LocalDateTime expiresOn;
+    private UserRole userRole;
+
+    public void Token(String id, User user, UserRole userRole, LocalDateTime expiresOn){
+        this.tokenId = id;
+        this.user = user;
+        this.userRole = userRole;
+        this.expiresOn = expiresOn;
+    }
+
+    public String getTokenId(){
+        return tokenId;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public boolean isUserInRole(UserRole userRole){
+        return this.userRole.equals(userRole);
+    }
+
+    public LocalDateTime getExpiresOn() {
+        return expiresOn;
+    }
 }
--- src/main/java/com/ge/academy/contact_list/repository/TokenRepository.java
+++ src/main/java/com/ge/academy/contact_list/repository/TokenRepository.java
@@ -2,15 +2,16 @@ package com.ge.academy.contact_list.repository;
 
 import com.ge.academy.contact_list.entity.Token;
 import com.ge.academy.contact_list.entity.UserRole;
+import com.ge.academy.contact_list.exception.EntityNotFoundException;
 
 /**
  * Created by 212566307 on 6/13/2016.
  */
 public interface TokenRepository {
 
-    public Token save(Token token);
-    public void delete(String tokenId);
-    public Token findOne(String tokenId);
+    public Token save(Token token) throws EntityNotFoundException;
+    public void delete(String tokenId) throws EntityNotFoundException;
+    public Token findOne(String tokenId) throws EntityNotFoundException;
     public boolean isUserInRole(UserRole userRole);
 
 }

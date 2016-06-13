f5ababcd4aeafd9ae787fb732ec4d76143486e61
Aron Horvath <aron.horvath@ge.com>
2016-06-13 13:29:36 +0200

    equals method for ContactGroup

--- src/main/java/com/ge/academy/contact_list/entity/ContactGroup.java
+++ src/main/java/com/ge/academy/contact_list/entity/ContactGroup.java
@@ -38,4 +38,21 @@ public class ContactGroup {
                 ", displayName= " + displayName +
                 '}';
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        ContactGroup that = (ContactGroup) o;
+
+        return name != null ? name.equals(that.name) : that.name == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = name != null ? name.hashCode() : 0;
+        result = 31 * result + (displayName != null ? displayName.hashCode() : 0);
+        return result;
+    }
 }

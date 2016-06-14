ed5ab5c1470fdc9dc8f927e7195bf507d10028a6
Andras Nemeth <andrasjanos.nemeth@ge.com>
2016-06-14 10:38:01 +0200

    logout

--- src/main/java/com/ge/academy/contact_list/service/AuthenticationService.java
+++ src/main/java/com/ge/academy/contact_list/service/AuthenticationService.java
@@ -2,6 +2,7 @@ package com.ge.academy.contact_list.service;
 
 import com.ge.academy.contact_list.entity.Token;
 import com.ge.academy.contact_list.exception.AuthenticationFailedException;
+import com.ge.academy.contact_list.exception.EntityNotFoundException;
 import com.ge.academy.contact_list.exception.TokenExpiredException;
 
 /**
@@ -11,4 +12,5 @@ public interface AuthenticationService {
 
     public Token findTokenById(String tokenId) throws TokenExpiredException;
     public Token authenticate(String username, String password) throws AuthenticationFailedException;
+    public Token logOut(String username) throws EntityNotFoundException;
 }
